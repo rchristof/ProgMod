310a351079a35e033fbe913dd2fc6d32b94ac1c2
rchristof <rafael.dana.christof@gmail.com>
2024-11-29 15:35:25 -0300

    feat: working conversion

--- return_messages/__init__.py
+++
@@ -1,1 +0,0 @@
-from .return_messages import *
--- teste.py
+++
@@ -1,9 +0,0 @@
-from operations import makeDeposit, makeTransfer, generateReport
-
-def teste():
-    makeDeposit("12345678900", "IBAN123", 100.0)
-    makeTransfer("12345678900", "98765432100", "IBAN123", "IBAN456", 50.0)
-    generateReport("12345678900", "IBAN123")
-
-if __name__ == "__main__":
-    teste()
