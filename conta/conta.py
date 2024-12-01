35171f8f77508663cca340dbd1c19a5b86abb637
FRANCESCO PUGLISI s332127 <s332127@studenti.polito.it>
2024-12-01 17:13:41 -0300

    feat: fix conta

--- conta/conta.py
+++ conta/conta.py
@@ -2,7 +2,7 @@ from user import *
 from return_messages import *
 import random
 
-__all__ = ["updateBalance", "verifyExistenceConta", "createNewConta", "verifyBalance", "getConta", "getIBAN"]
+__all__ = ["updateBalance", "verifyExistenceConta", "createNewConta", "verifyBalance", "getConta"]
 
 contas = []
 
