8f0e75a8e647892b819c22a7304f03a4726a25b3
Vinícius Martins <149122682+viniciusmartinsr@users.noreply.github.com>
2024-12-01 19:39:41 -0300

    changing test_conta

--- conta/test_conta.py
+++ conta/test_conta.py
@@ -1,64 +1,80 @@
 import unittest
-from conta.conta import *
-from conta import contas
+from conta import *
+from user import *
+from return_messages import *
 
-# Função para limpar o estado global antes de cada teste
-def setup():
-    contas.clear()
+contas = []
+userTest = {"name": "Pedro", "surname": "Rossi", "cpf": "12345678910", "password": "mariorossi1234"}
+contas_test1 = {"CPF": 16557283774, "IBAN": 100000, "balance": 20}
 
-# Funções de teste
-def test_create_new_account():
-    setup()  # Limpa a lista de contas antes do teste
-    valid_cpf = "12345678900"
-    response = createNewConta(valid_cpf)
-    assert response['code'] == 0, "Failed to create a new account"
+class TestUser(unittest.TestCase):
+    def test_01_createNewAccount_1(self):
+        print("Test Case 01 - Creating a new account with sucess")
+        ret_val = createNewUser(userTest["name"], userTest["surname"], userTest["cpf"], userTest["password"])
+        self.assertEqual(ret_val, msg_success)
+    def test_02_createNewAccount_2(self):
+        print("Test Case 02 - Creating a new account ")
+        ret_val = createNewUser(userTest["name"], userTest["surname"], userTest["cpf"], userTest["password"])
+        self.assertEqual(ret_val, msg_err_userAlreadyExists)
+    def test_03_createNewAccount_3(self):
+        print("Test Case 03 - Creating a new account ")
+        cpf = "12345678910"
+        ret_val = createNewConta(cpf)
+        self.assertEqual(ret_val, msg_err_contaAlreadyExists)
+    def test_04_verifyExistenceConta_1(self):
+        print("Test Case 04 - Verifying Existence Conta with a wrong cpf")
+        cpf = "12345"
+        ret_val = verifyExistenceConta(cpf)
+        self.assertEqual(ret_val, msg_err_invalidCpf)
+    def test_05_verifyExistenceConta_2(self):
+        print("Test Case 05 - Verifying Existence Conta that not exists")
+        cpf = "12345678912"
+        ret_val = verifyExistenceConta(cpf)
+        self.assertEqual(ret_val, msg_err_contaNotExists)
+    def test_06_verifyExistenceConta_3(self):
+        print("Test Case 06 - Verifying Existence Conta that exists")
+        cpf = "12345678910"
+        ret_val = verifyExistenceConta(cpf)
+        self.assertEqual(ret_val, msg_success)
+    def test_07_verifyBalance_1(self):
+        print("Test Case 07 - Verifying Balance with a wrong val")
+        cpf = "12345678912"
+        iban = "1222222222"
+        val = -2
+        ret_val = verifyBalance(cpf, iban, val)
+        self.assertEqual(ret_val, msg_err_invalidVal)
+    def test_08_verifyBalance_2(self):
+        print("Test Case 08 - Verifying Balance with a wrong iban")
+        cpf = "12345678912"
+        iban = 12345678
+        val = 100
+        ret_val = verifyBalance(cpf, iban, val)
+        self.assertEqual(ret_val, msg_err_contaNotExists)
+    def test_09_verifyBalance_3(self):
+        print("Test Case 09 - Verifying Balance with a wrong iban")
+        #falta terminar
+    def test_10_getConta_01(self):
+        print("Test Case 10 - Verifying getConta with an unexistense conta")
+        cpf = "15448759778"
+        ret_val = getConta(cpf)
+        self.assertEqual(ret_val, msg_err_contaNotExists)
+    def test_11_verifyFormatIban_01(self):
+        print("Test Case 11 - Verifying format IBAN with a correct format") 
+        iban = 12345678
+        ret_val = verifyFormatIban(iban)
+        self.assertEqual(ret_val, msg_success)
+    def test_12_verifyFormatIban_02(self):
+        print("Test Case 12 - Verifying format IBAN with a incorrect format (less numbers)") 
+        iban = 123456
+        ret_val = verifyFormatIban(iban)
+        self.assertEqual(ret_val, msg_err_invalidIban)
+    def test_13_verifyFormatIban_03(self):
+        print("Test Case 13 - Verifying format IBAN with a incorrect format (string)")
+        iban = "12345678"
+        ret_val = verifyFormatIban(iban)
+        self.assertEqual(ret_val, msg_err_invalidIban)
+    #def test_14_updateBalance_01(self):
 
-def test_update_balance_success():
-    setup()
-    valid_cpf = "12345678900"
-    createNewConta(valid_cpf)
-    valid_iban = contas[0]['IBAN']
-    value = 100
-    response = updateBalance(valid_cpf, valid_iban, value)
-    assert response['code'] == 0, "Failed to update balance"
-    assert contas[0]['balance'] == 100, "Balance mismatch after update"
 
-def test_update_balance_invalid_iban():
-    setup()
-    valid_cpf = "12345678900"
-    invalid_iban = "1234567"
-    value = 100
-    response = updateBalance(valid_cpf, invalid_iban, value)
-    assert response['code'] == 4, "Invalid IBAN not detected"
-
-def test_verify_account_existence_success():
-    setup()
-    valid_cpf = "12345678900"
-    createNewConta(valid_cpf)
-    valid_iban = contas[0]['IBAN']
-    response = verifyExistenceConta(valid_cpf, valid_iban)
-    assert response == 0, "Failed to verify account existence"
-
-def test_verify_balance_insufficient_funds():
-    setup()
-    valid_cpf = "12345678900"
-    createNewConta(valid_cpf)
-    valid_iban = contas[0]['IBAN']
-    value = 1000000
-    response = verifyBalance(valid_cpf, valid_iban, value)
-    assert response == 6, "Insufficient funds not detected"
-
-# Registrando testes na Test Suite
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.FunctionTestCase(test_create_new_account))
-    suite.addTest(unittest.FunctionTestCase(test_update_balance_success))
-    suite.addTest(unittest.FunctionTestCase(test_update_balance_invalid_iban))
-    suite.addTest(unittest.FunctionTestCase(test_verify_account_existence_success))
-    suite.addTest(unittest.FunctionTestCase(test_verify_balance_insufficient_funds))
-    return suite
-
-# Executando os testes
 if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
+    unittest.main()
